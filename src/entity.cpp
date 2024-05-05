b1814bad0a7a4f62f16d8eef09a0918b2db53f48
Анастасия Малышева <anastasiamalysheva250@gmail.com>
2024-05-05 21:07:52 +0300

    saving changings of entity before pulling nastia_develop

---
+++ src/entity.cpp
@@ -0,0 +1,17 @@
+#ifndef ENTITY_CPP
+#define ENTITY_CPP
+#include "entity.hpp"
+
+namespace super_hse{
+void entity::setStatus(EntityStatus status_){
+    status = status_;
+}
+
+EntityStatus entity::getStatus(){
+    return status;
+}
+
+}
+
+
+#endif
