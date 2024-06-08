1f1f8e0a60a552d87785e4110079ab20ca66cef7
julia <julia.b.omsk@gmail.com>
2024-06-08 22:20:09 +0300

    tests etc

--- tests/LevelTest.cpp
+++ tests/LevelTest.cpp
@@ -1,57 +1,69 @@
 #ifndef LEVEL_TEST_CPP
 #define LEVEL_TEST_CPP
 
-#include "level_info.hpp"
-#include "Level.hpp"
 #include <cassert>
 #include <exception>
+#include <filesystem>
+#include <iostream>
 #include <stdexcept>
 #include <string>
-#include <iostream>
-#include <filesystem>
+#include "Level.hpp"
+#include "level_info.hpp"
 
-namespace super_hse{
+namespace super_hse {
 
 static std::filesystem::path p(std::filesystem::current_path());
 
-void checkLevelInfoConstructor(){
+void checkLevelInfoConstructor() {
     LevelInfo levelInfoEmpty;
     assert(levelInfoEmpty.colliderName.size() == 0);
     assert(levelInfoEmpty.entityLayerName.size() == 0);
     assert(levelInfoEmpty.tileLayerName.size() == 0);
-    LevelInfo levelInfoTestFile(p.parent_path().parent_path().string() + "/assets/files/level1_test.txt");
-    assert(levelInfoTestFile.filename == "../assets/tilemaps/first_level2.ldtk");
+    LevelInfo levelInfoTestFile(
+        p.parent_path().parent_path().string() + "/assets/files/level1_test.txt"
+    );
+    assert(
+        levelInfoTestFile.filename == "../assets/tilemaps/first_level2.ldtk"
+    );
     assert(levelInfoTestFile.tileLayerName.size() == 5);
     assert(levelInfoTestFile.entityLayerName.size() == 1);
     assert(levelInfoTestFile.colliderName.size() == 2);
 
     std::string descriptionFile = "wtf52";
-    std::string excpectedErrorMessage = "Cannot find \'" + descriptionFile + "\' description file\n";
+    std::string excpectedErrorMessage =
+        "Cannot find \'" + descriptionFile + "\' description file\n";
     try {
         LevelInfo levelInfoWrongFile(descriptionFile);
-    }catch (std::runtime_error  &e){
+    } catch (std::runtime_error &e) {
         assert(e.what() == excpectedErrorMessage);
     }
-
 }
 
-void checkLevelConstructor(){
-    Level levelFromRightFileName(p.parent_path().parent_path().string() + "/assets/tilemaps/first_level2.ldtk");
-    assert(levelFromRightFileName.ldtk_filename == p.parent_path().parent_path().string() +  "assets/tilemaps/first_level2.ldtk");
+void checkLevelConstructor() {
+    Level levelFromRightFileName(
+        p.parent_path().parent_path().string() +
+        "/assets/tilemaps/first_level2.ldtk"
+    );
+    assert(
+        levelFromRightFileName.ldtk_filename ==
+        p.parent_path().parent_path().string() +
+            "/assets/tilemaps/first_level2.ldtk"
+    );
 
     std::string projectName = "wtf52";
-    std::string expectedErrorMessage = "Cannot find \'" + projectName + "\'' project\n";
-    try{
+    std::string expectedErrorMessage =
+        "Cannot find \'" + projectName + "\'' project\n";
+    try {
         Level levelWithWrongFileName(projectName);
-    }catch(ldtkException &e){
+    } catch (ldtkException &e) {
         assert(e.what() == expectedErrorMessage);
     }
 }
 
-void level_test(){
+void level_test() {
     super_hse::checkLevelInfoConstructor();
     super_hse::checkLevelConstructor();
 }
 
-}
+}  // namespace super_hse
 #endif
